7ee02f6abe5c7476627b2049faeafd56e8a64dd4
phan cuong <phanxuancuong00677@gmail.com>
2023-12-03 22:34:50 +0700

    done

--- JSX/BAI_YourName/your-name1/src/index.js
+++ JSX/BAI_YourName/your-name1/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//bai 1:
+// import React from "react";
+// import ReactDom from "react-dom/client";
+// const name = "Your name";
+// const root = ReactDom.createRoot(document.getElementById("root"));
+// root.render(
+//   React.createElement("h1",{style: {color: "red"}}, name)
+// )
+//bai 2:nhung ten ban 
+// import ReactDom from "react-dom/client";
+// const name = "Your name";
+// const root = ReactDom.createRoot(document.getElementById("root"));
+// root.render(
+//   <h1 style={{textAlign: "center" ,color: "red"}}> 
+//     {name}
+//   </h1>
+// )
+//bai 3: tao them DANH SACH
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const fruits = ['Apple', 'Banana', 'Orange', 'Apricot', 'Black rowan', 'Cranberry']
+
+ReactDOM.render(
+<div>
+    <h1>List of fruits</h1>
+    <ul>
+      { fruits.map((item) => (
+        <li>{ item }</li>
+      )) }
+    </ul>
+ </div>,
+document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
